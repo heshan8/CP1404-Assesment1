ce6418f5b78355e4adac5e95e3487a27edc3ca6a
Heshan <heshan988@gmail.com>
2017-04-13 00:37:21 +1000

    functions added

--- assignment1.py
+++ assignment1.py
@@ -12,6 +12,7 @@ def main():
     with open('books.csv', 'r') as c:
         edit = csv.reader(c)
         reading_list = list(edit)
+        reading_list.sort()
     print("Reading List 1.0 - by Tharindu Heshan")
     print(MAIN_MENU)
     menu_choice = input("Enter choice: ").upper()
@@ -28,10 +29,38 @@ def main():
             print("Invalid input, Try again.")
         print(MAIN_MENU)
         menu_choice = input("Enter choice: ").upper()
-    with open("books.csv", "a", newline='') as c:
+    with open("books.csv", "w", newline='') as c:
         edit = csv.writer(c)
         edit.writerows(reading_list)
-    print("Have a nice day :)")
+    if menu_choice == "Q":
+        print("Have a nice day :)")
+        exit
+
+
+def show_list(reading_list, books):
+    if books == "r":
+        print("List of required books:")
+    elif books == "c":
+        print("List of completed books")
+    elif books == "m":
+        books = "r"
+    count = 0
+    total = 0
+    for item in reading_list:
+        if books in item[3]:
+            print("{}. {:20}   $ {:10} ({})".format(count, item[0], item[1], item[2], item[3]))
+            count += 1
+            total += float(item[1])
+
+    if count == 0:
+        if books == "r":
+            print("No required books to display")
+        elif books == "c":
+            print("No completed items to display")
+        else:
+            print("No items exist in the list")
+    else:
+        print("Total expected price for {} items: ${}".format(count, total))
 
 
 
