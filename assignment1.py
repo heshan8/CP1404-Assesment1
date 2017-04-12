3258ccb7248d1c28388810a8f58bc6b5dea56dcc
Heshan <heshan988@gmail.com>
2017-04-12 20:05:15 +1000

    Minor Update

--- assignment1.py
+++ assignment1.py
@@ -1,44 +1,48 @@
 """
-Replace the contents of this module docstring with your own details
+CP1404 Assignment -1-
+Reading list by Tharindu Heshan
+Add github link
 """
+main_menu = "\nMain Menu:\n R - Show list of required books\n C - Show list of completed books\n" \
+            " A - Add a new book\n M - Mark a book as completed\n Q - Quit"
+
 import csv
 
 def main():
-    print("Reading List 1.0 - by <Tharindu Heshan>")
-    print()
-    choice = True
-    main_menu()
-    choice = str(input())
-    menu_choice = str(input())
-    if menu_choice.lower() == 'r':
-        print("Required Books:")
-        required_items()
-    elif menu_choice.lower() == 'c':
-        print("Completed Items:")
-        list_items_completed()
-    elif menu_choice.lower() == 'm':
-        list_items_required()
-        mark_item_completed()
-    elif menu_choice.lower() == 'a':
-        add_item()
-    elif menu_choice.lower() == 'q':
-        loop = False
-    else:
-        input("Input Error! Enter any key to try again.")
+    with open('books.csv', 'r') as f:
+        edit = csv.reader(f)
+        reading_list = list(edit)
+        reading_list.sort()
+    print("Reading List 1.0 - by Tharindu Heshan")
+    print(main_menu)
 
-
-print("Program exiting")
-
-def required_items():
-
-
- def main_menu():
-    print('Menu:')
-    print('R - List required books')
-    print('C - List completed books')
-    print('A - Add new book')
-    print('M - Mark a book as completed')
-    print('Q - Quit')
-    choice = str(input('Pick a choice from above: '))
 main()
+#     choice = True
+#     (main_menu)
+#     choice = str(input())
+#     menu_choice = str(input())
+#     if menu_choice.lower() == 'r':
+#         print("Required Books:")
+#         required_items()
+#     elif menu_choice.lower() == 'c':
+#         print("Completed Items:")
+#         list_items_completed()
+#     elif menu_choice.lower() == 'm':
+#         list_items_required()
+#         mark_item_completed()
+#     elif menu_choice.lower() == 'a':
+#         add_item()
+#     elif menu_choice.lower() == 'q':
+#         loop = False
+#     else:
+#         input("Input Error! Enter any key to try again.")
+#
+#
+# print("Program exiting")
+#
+# def required_items():
+
+
+
+
 
